e07ed6c7c91b34e157bec39e03c329902c6c6fdd
Kamonpat Sunthonpong <kamonpat.sunthonpong@scbtechx.io>
2024-07-09 09:01:07 +0700

    feat: add custom lsp

--- lua/autocmds.lua
+++ lua/autocmds.lua
@@ -5,9 +5,21 @@
 --  Try it with `yap` in normal mode
 --  See `:help vim.highlight.on_yank()`
 vim.api.nvim_create_autocmd('TextYankPost', {
-    desc = 'Highlight when yanking (copying) text',
-    group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
-    callback = function()
-      vim.highlight.on_yank()
-    end,
-  })
+  desc = 'Highlight when yanking (copying) text',
+  group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
+  callback = function()
+    vim.highlight.on_yank()
+  end,
+})
+
+vim.api.nvim_create_user_command('ListLspClients', function()
+  local clients = vim.lsp.get_clients()
+  if next(clients) == nil then
+    print 'No LSP client is attached to the buffer'
+  else
+    for _, client in pairs(clients) do
+      print('LSP client attached: ' .. client.name)
+    end
+  end
+end, {})
+
--- lua/lsp-setup.lua
+++ lua/lsp-setup.lua
@@ -138,18 +138,8 @@ capabilities = vim.tbl_deep_extend('force', capabilities, require('cmp_nvim_lsp'
 --  - settings (table): Override the default settings passed when initializing the server.
 --        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
 local servers = {
-  -- clangd = {},
   gopls = {},
-  -- pyright = {},
   rust_analyzer = {},
-  -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
-  --
-  -- Some languages (like typescript) have entire language plugins that can be useful:
-  --    https://github.com/pmizio/typescript-tools.nvim
-  --
-  -- But for many setups, the LSP (`tsserver`) will work just fine
-  -- tsserver = {},
-  --
 
   lua_ls = {
     -- cmd = {...},
@@ -195,3 +185,22 @@ require('mason-lspconfig').setup {
     end,
   },
 }
+
+local client = vim.lsp.start_client {
+  name = 'jclsp',
+  cmd = { '/Users/kamonpatsunthopong/_workspace/_learning/golang/jenkins-share-lib-lsp/main' },
+  -- on_attach = require('lsp-setup').on_attach,
+}
+
+if not client then
+  vim.notify 'Something wrong with jcllsp'
+  return
+end
+
+vim.api.nvim_create_autocmd('FileType', {
+  pattern = 'groovy',
+  callback = function()
+    vim.lsp.buf_attach_client(0, client)
+    print 'start jclsp'
+  end,
+})
