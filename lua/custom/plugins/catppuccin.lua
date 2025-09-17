aceb67c1de248a46cdb28c07a0acae2992e1b180
Akaifox16 <kamonpat.sun@outlook.com>
2025-09-17 21:13:49 +0700

    update catppuccin integration

--- lua/custom/plugins/catppuccin.lua
+++ lua/custom/plugins/catppuccin.lua
@@ -1,17 +1,30 @@
 return {
-    'catppuccin/nvim',
-    name = 'catppuccin',
-    priority = 1000,
+  'catppuccin/nvim',
+  name = 'catppuccin',
+  priority = 1000,
 
-    opts = {
-        background = {
-            dark = 'macchiato'
-        }
+  opts = {
+    background = {
+      dark = 'macchiato',
     },
+    transparent_background = true,
+    integrations = {
+      cmp = true,
+      gitsigns = true,
+      treesitter = true,
+      which_key = true,
+      lsp_trouble = true,
+      telescope = {
+        enabled = true,
+      },
+      mason = true,
+      harpoon = true,
+    },
+  },
 
-    config = function()
-        vim.cmd.colorscheme('catppuccin')
+  config = function()
+    vim.cmd.colorscheme 'catppuccin'
 
-        vim.cmd.hi 'Comment gui=none'
-    end
+    vim.cmd.hi 'Comment gui=none'
+  end,
 }
